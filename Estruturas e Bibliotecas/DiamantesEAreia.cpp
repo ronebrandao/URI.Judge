839a3b3da054eaf440955535c2308610bd0763f8
Rone Filho <rone_filho@hotmail.com>
2019-04-16 15:57:40 -0300

    Fix bug

--- Estruturas e Bibliotecas/DiamantesEAreia.cpp
+++ Estruturas e Bibliotecas/DiamantesEAreia.cpp
@@ -8,41 +8,40 @@ using namespace std;
 int main()
 {
     string entry;
-    
-    while (getline(cin, entry))
+    int nc;
+    scanf("%d ", &nc);
+
+    while (nc--)
     {
-        if (isdigit(entry[0])) continue;
-        
+
+        getline(cin, entry);
         bool valid = false;
         vector<char> left;
         int num = 0;
-        
+
         for (int i = 0; i < entry.size(); i++)
         {
-            
+
             if (entry[i] == '>')
             {
                 if (left.empty())
                 {
-                    valid = false;   
+                    valid = false;
                 }
-                else 
+                else
                 {
                     left.pop_back();
                     num++;
                     valid = true;
                 }
-                    
             }
             else if (entry[i] == '<')
             {
                 left.push_back(entry[i]);
             }
-            
         }
-        
+
         left.clear();
         cout << num << endl;
-        
     }
 }
